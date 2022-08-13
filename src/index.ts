aa4ab503f2cad8a48ad5200e90569f1959a4d72f
Gideon Ritter <gideon.ritter@enymo.com>
2022-08-13 12:19:50 +0200

    Minor improvement.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { MutableRefObject, useEffect } from "react";
+import { MutableRefObject, useCallback, useEffect } from "react";
 
 export default function useOnClickOutside<T extends HTMLElement>(ref: MutableRefObject<T>, onClick: (event: MouseEvent) => void) {
-    const handleEvent = (event: MouseEvent) => {
+    const handleEvent = useCallback((event: MouseEvent) => {
         if (!ref.current?.contains(event.target as HTMLElement)) {
             onClick(event);
         }
-    }
+    }, [ref, onClick])
 
     useEffect(() => {
         window.addEventListener("click", handleEvent);
         return () => window.removeEventListener("click", handleEvent);
-    }, []);
+    }, [handleEvent]);
 }
