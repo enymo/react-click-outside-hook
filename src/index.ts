64a92ed86d5b19e41974c6886f5dd59ddb963b4d
Gideon Ritter <gideon.ritter@enymo.com>
2023-09-17 18:57:09 +0200

    Fix null safety.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from "react";
 
 export default function useOnClickOutside<T extends HTMLElement>(onClick: (event: MouseEvent) => void, dependencies: React.DependencyList = []) {
-    const ref = useRef<T>();
+    const ref = useRef<T>(null);
 
     useEffect(() => {
         const handleEvent = (event: MouseEvent) => {
